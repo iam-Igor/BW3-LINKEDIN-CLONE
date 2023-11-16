9796a05d64e695226769c385bec8c3532e2c78d8
Ygor Garofalo <igorgarofalo@gmail.com>
2023-11-16 15:06:56 +0100

    fixes

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -6,6 +6,7 @@ export const PARAMS = "PARAMS";
 export const SEARCH = "SEARCH";
 export const SET_SEARCH = "SET_SEARCH";
 export const LOADING = "LOADING";
+export const APIKEY = "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";
 
 const store = configureStore({
   reducer: mainReducer,
