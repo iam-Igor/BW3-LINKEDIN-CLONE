008527b5776fd3dbff471efdf8d37841b49a2f5a
guimasi1 <143355576+guimasi1@users.noreply.github.com>
2023-11-16 10:40:36 +0100

    corrects typo

--- src/components/GoUpButton.jsx
+++ src/components/GoUpButton.jsx
@@ -9,7 +9,7 @@ const GoUpButton = () => {
   };
   return (
     <Button onClick={scrollToTop} className="rounded-circle fs-4 go-up">
-      <i class="bi bi-arrow-up-circle-fill"></i>
+      <i className="bi bi-arrow-up-circle-fill"></i>
     </Button>
   );
 };
